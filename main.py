93205ad0c501efc4c980a8ee7d6a7d1f5f7d89c9
t.setoguchi <t.setoguchi2611@gmail.com>
2026-09-02 20:02:16 +0900

    feat:読み込んた.envのデータを使用してemailを送信する処理とエラーハンドリングを追加

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import smtplib
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,6 +10,18 @@ SENDER_MAIL = os.getenv('SENDER_EMAIL')
 SENDER_PASS = os.getenv('SENDER_PASSWORD')
 RECEIVER_MAIL = os.getenv('RECEIVER_EMAIL')
 
-print(SENDER_MAIL)
-print(SENDER_PASS)
-print(RECEIVER_MAIL)
+msg = MIMEText("メール本文をここに書き込む", "plain", "utf-8")
+msg["Subject"] = "件名"
+msg["From"] = SENDER_MAIL
+msg["To"] = RECEIVER_MAIL
+
+
+try:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(SENDER_MAIL, SENDER_PASS)
+        server.sendmail(SENDER_MAIL, RECEIVER_MAIL, msg.as_string())
+    print("メールを送信しました")
+
+except Exception as e:
+    print(f"送信失敗: {e}")
+    sys.exit()
