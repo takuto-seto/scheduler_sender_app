9ce244230a86d7e3839566b35a5272f45c80d5e3
t.setoguchi <t.setoguchi2611@gmail.com>
2026-09-02 20:38:20 +0900

    feat:読み取ったJSONデータから東京の天気だけを抽出する処理を追加

--- weather.py
+++ weather.py
@@ -7,7 +7,16 @@ response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    print(data)
+
+    # tokyo_areas = data[0]['timeSeries'][0]['areas'][0]
+    # tokyo_weather = tokyo_areas['weathers'][0]
+    # print(f"トウキョウエリアの天気：{tokyo_areas}")
+    # print(f"トウキョウの天気：{tokyo_weather}")
+
+    tokyo_area_temp = data[0]['timeSeries'][2]['areas'][0]
+    tokyo_temp = tokyo_area_temp['temps']
+    print(tokyo_area_temp)
+    print(tokyo_temp)
 
 else:
     print("dataの取得に失敗しました")
