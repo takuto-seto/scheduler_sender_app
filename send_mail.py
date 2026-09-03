34512e3dde7d1f70d3fbcbefc73865a915c30f1d
t.setoguchi <t.setoguchi2611@gmail.com>
2026-09-03 20:46:09 +0900

    feat:weather.pyの処理を関数化してsend_mail.pyとweather.pyを統合。東京の天気予報がメールで送られてくることを確認した

--- send_mail.py
+++ send_mail.py
@@ -4,14 +4,17 @@ import smtplib
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 
+from weather import get_tokyo_weather
+
+weather_info, temp = get_tokyo_weather()
 load_dotenv()
 
 SENDER_MAIL = os.getenv('SENDER_EMAIL')
 SENDER_PASS = os.getenv('SENDER_PASSWORD')
 RECEIVER_MAIL = os.getenv('RECEIVER_EMAIL')
 
-msg = MIMEText("メール本文をここに書き込む", "plain", "utf-8")
-msg["Subject"] = "件名"
+msg = MIMEText(f"東京の天気は{weather_info} `予想気温は{temp}です", "plain", "utf-8")
+msg["Subject"] = "天気予報のお知らせ"
 msg["From"] = SENDER_MAIL
 msg["To"] = RECEIVER_MAIL
 
--- weather.py
+++ weather.py
@@ -1,22 +1,33 @@
 import requests
+import sys
+import logging
 
-region_code = "130000"
-url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{region_code}.json"
+logging.basicConfig(
+    filename='log/get_tokyo_weather.log',
+    format='%(asctime)s %(levelname)s %(message)s ',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:M:S'
+)
 
-response = requests.get(url)
+def get_tokyo_weather():
 
-if response.status_code == 200:
-    data = response.json()
 
-    # tokyo_areas = data[0]['timeSeries'][0]['areas'][0]
-    # tokyo_weather = tokyo_areas['weathers'][0]
-    # print(f"トウキョウエリアの天気：{tokyo_areas}")
-    # print(f"トウキョウの天気：{tokyo_weather}")
+    region_code = "130000"
+    url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{region_code}.json"
 
-    tokyo_area_temp = data[0]['timeSeries'][2]['areas'][0]
-    tokyo_temp = tokyo_area_temp['temps']
-    print(tokyo_area_temp)
-    print(tokyo_temp)
+    response = requests.get(url)
 
-else:
-    print("dataの取得に失敗しました")
+    if response.status_code == 200:
+        data = response.json()
+
+        tokyo_areas = data[0]['timeSeries'][0]['areas'][0]
+        tokyo_weather = tokyo_areas['weathers'][0]
+
+        tokyo_area_temp = data[0]['timeSeries'][2]['areas'][0]
+        tokyo_temp = tokyo_area_temp['temps']
+
+        return f"{tokyo_weather}", f"{tokyo_temp}"
+
+    else:
+        logging.error("dataの取得に失敗しました")
+        sys.exit()
