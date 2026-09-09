c8dd30869a4777be92c00bf54a6db835e1525fa7
t.setoguchi <t.setoguchi2611@gmail.com>
2026-09-09 19:10:20 +0900

    feat:mockにfixtureを使用してテストコードの準備にかかる時間を削減

--- test_weather.py
+++ test_weather.py
@@ -2,40 +2,69 @@ from unittest.mock import Mock, patch
 import pytest
 from weather import get_tokyo_weather
 
-
-@patch('weather.requests.get')
-def test_get_tokyo_weather(mock_get):
+@pytest.fixture
+def mock_weather_responce():
     mock_responce = Mock()
     mock_responce.status_code = 200
     mock_responce.json.return_value = [
-    {
-        "timeSeries": [
         {
-            "areas": [
-            {
-                "weathers": ["晴れ"]
-            }
-            ]
-        },
-        {
-            "areas": [
-            {
-                "unknown": ["unknown"]         
-            }
-            ]
-        },
-        {
-            "areas": [
-            {
-                "temps": ["25"]
-            }
-            ]
+            "timeSeries": [
+                {
+                    "areas": [
+                        {"weathers": ["晴れ"]}
+                        ]
+                },
+                {
+                    "areas": [
+                        {"unknown": ["unknown"]}
+                        ]
+                },
+                {
+                    "areas": [
+                        {"temps": ["25"]}
+                        ]
+                }
+                ]
         }
-        ]
-    }
     ]
 
-    mock_get.return_value = mock_responce
+    return mock_responce
+    
+
+
+@patch('weather.requests.get')
+def test_get_tokyo_weather(mock_get, mock_weather_responce):
+    # mock_responce = Mock()
+    # mock_responce.status_code = 200
+    # mock_responce.json.return_value = [
+    # {
+    #     "timeSeries": [
+    #     {
+    #         "areas": [
+    #         {
+    #             "weathers": ["晴れ"]
+    #         }
+    #         ]
+    #     },
+    #     {
+    #         "areas": [
+    #         {
+    #             "unknown": ["unknown"]         
+    #         }
+    #         ]
+    #     },
+    #     {
+    #         "areas": [
+    #         {
+    #             "temps": ["25"]
+    #         }
+    #         ]
+    #     }
+    #     ]
+    # }
+    # ]
+
+    mock_get.return_value = mock_weather_responce
 
     weather, temp = get_tokyo_weather()
 
