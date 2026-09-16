282b012c1731eda5cf782dd4916cc7db00c06646
t.setoguchi <t.setoguchi2611@gmail.com>
2026-09-16 20:29:32 +0900

    feat:SMTP_SSLをモック化。テストでメール送信しないよう変更した

--- test_send_mail.py
+++ test_send_mail.py
@@ -8,11 +8,17 @@ def mock_weather_data():
     return "晴れ", "25℃"
 
 @patch('send_mail.get_tokyo_weather')
-def test_send_weather_mail_success(mock_get_weather, mock_weather_data):
+@patch('send_mail.smtplib.SMTP_SSL')
+def test_send_weather_mail_success(mock_smtp, mock_get_weather, mock_weather_data):
     mock_get_weather.return_value = mock_weather_data
+
+    # SMTP_SSLのモック設定
+    mock_server = Mock()
+    mock_smtp.return_value.__enter__.return_value = mock_server
 
     send_weather_mail()
 
     mock_get_weather.assert_called_once()
+    mock_server.sendmail.assert_called_once()
 
     
